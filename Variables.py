d67bd8aed08b257cdfe10bcf3b82f9ead4c1a81b
Sasi Kumar <140006101+Sasikumar97fl@users.noreply.github.com>
2023-08-14 21:10:28 +0530

    Variables - Basics

---
+++ Variables.py
@@ -0,0 +1,68 @@
+'''Variables'''
+
+# An integer assignment
+age = 45
+# A floating point
+salary = 1456.8
+# A string
+name = "John"
+# printing statement
+print(age)
+print(salary)
+print(name)
+
+# Re-declare the variable
+Number=100
+# display
+print(Number) #"Before RE declare:",
+# re-declare the var
+Number=120.3
+print(Number) #"After re-declare:",
+
+# Assigning different values to multiple variables:
+a,b,c = (1,20.2,"Learn Python in Tamil")
+print(a)
+print(b)
+print(c)
+
+# String - is a single, double and triple quote
+name = "Data Science"
+Name = 'Data Science'
+university = """Anna University"""
+University = '''Anna University'''
+
+location = '''Anna University is located in chennai'''
+
+print(name) 
+print(Name)
+print(university) 
+print(University)
+print(location)
+
+
+""" Python Data Types
+Numeric - Integer , Float, Complex.
+Boolean
+Set
+Mapping (dictionary)
+Sequence - String, List and Tuple """
+
+""" x = "Hello Python" #str
+x = 21             #int
+x = 202.5          #float
+x = 3j             #complex
+x = True           #bool
+
+x = ["apple", "banana", "cherry"]   #list
+x = ("apple", "banana", "cherry")   #tuple
+x = {"name" : "John", "age" :36}    #dict
+x = {"apple", "banana", "cherry"}   #set
+x = ({"apple", "banana", "cherry"}) #frozenset """
+
+Everyone = "please slient"
+print(Everyone)
+'''hi'''
+
+
+
+
